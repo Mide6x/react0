53396a7e8ea85cd0bd47f5bf6b43d7e8b7a2c1b4
Olumide <adewoleolumide05@gmail.com>
2024-05-04 21:13:30 +0100

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+const welcome = {
+  greeting: "you",
+  user: "are welcome",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello, {welcome.greeting} {welcome.user}
+      </h1>
     </div>
   );
 }
